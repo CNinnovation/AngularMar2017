8bb996140e6bda51bc93b63470cc96389284a7fa
Christian Nagel <christian@christiannagel.com>
2017-03-30 13:28:00 +0200

    ASP.NET Web API with EF

---
+++ MyWebAPIDatabaseSample/MyWebAPIDatabaseSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebAPIDatabaseSample.Startup))]
+namespace MyWebAPIDatabaseSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
